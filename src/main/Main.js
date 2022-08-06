f7c264e3ea5a82c3ec35d01d569741b2c89fcbf5
StepashkaSuicide <churbakov.evgen@mail.ru>
2022-08-06 17:58:53 +0300

    1 part complete

--- src/main/Main.js
+++ src/main/Main.js
@@ -6,14 +6,16 @@ export const Main = () => {
     return (
         <div className={s.mainBlock}>
 
-            <div className={sContainer.container}>
+            <div className={`${sContainer.container} ${s.mainBlock}`}>
                 <div className={s.text}>
                     <span>Hi There</span>
                     <h1>I am Evgeniy</h1>
                     <p>Frontend Developer</p>
                 </div>
                 <div className={s.photo}>
-
+                    <img
+                        src="https://sun9-east.userapi.com/sun9-32/s/v1/if1/cS6h1CtGV2Pwti0rCP6cJD3U2JIYi6oy_5IFO6q1Hrj4Roba5i3T1OPHn85LxbISNjjYlJAd.jpg?size=2560x1920&quality=96&type=album"
+                        alt="fff"/>
                 </div>
             </div>
 
