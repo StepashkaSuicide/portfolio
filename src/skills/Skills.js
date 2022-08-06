02d58b5a252053fc5bde3cc23d0a467925f88362
StepashkaSuicide <churbakov.evgen@mail.ru>
2022-08-06 17:01:56 +0300

    1 part complete

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -3,15 +3,29 @@ import s from './Skills.module.css';
 import sContainer from './../common/styles/Container.module.css'
 import {Skill} from "./skill/Skill";
 
+const arr = [
+    {
+    title: 'JS',
+    description:'od JS good ',
+},
+     {
+    title: 'CSS',
+    description:'JS its very good JS its d JSits very good',
+},
+     {
+    title: 'REACT',
+    description:'od Jy good CSS very good CSS veS good ',
+},
+
+]
+
 export const Skills = () => {
     return (
         <div className={s.skillsBlock}>
             <div className={`${sContainer.container} ${s.skillsContainer}`}>
                 <h2 className={s.title}>Skills</h2>
                 <div className={s.skills}>
-                    <Skill title={'JS'} description={'JS its very good JS its very good JS its very good JS its very good JS its very good JS its very good '}/>
-                    <Skill title={'CSS'} description={'CSS very good CSS very good CSS very good CSS very good CSS very good CSS very good CSS very good CSS very good CSS very good CSS very good '}/>
-                    <Skill title={'REACT'} description={'React insane  React insane  React insane  React insane  React insane  React insane  React insane  React insane  React insane  React insane  '}/>
+                    {arr.map(t=> <Skill key={t.title} title={t.title} description={t.description}/>)}
                 </div>
             </div>
         </div>
