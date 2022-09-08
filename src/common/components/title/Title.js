db0d490b3d934a209ac9cebd1e69b3c15f50852a
StepashkaSuicide <churbakov.evgen@mail.ru>
2022-09-08 07:27:17 +0300

    2 part 23%

---
+++ src/common/components/title/Title.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import s from './Title.module.css';
+
+
+export const Title = (props) => {
+    return (
+        <div className={s.title}>
+            <h2>{props.title}</h2>
+        </div>
+    );
+};
--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -2,6 +2,7 @@ import React from 'react';
 import s from './Skills.module.css';
 import sContainer from './../common/styles/Container.module.css'
 import {Skill} from "./skill/Skill";
+import {Title} from "../common/components/title/Title";
 
 const arr = [
     {
@@ -23,7 +24,7 @@ export const Skills = () => {
     return (
         <div className={s.skillsBlock}>
             <div className={`${sContainer.container} ${s.skillsContainer}`}>
-                <h2 className={s.title}>Skills</h2>
+                <Title title={'Skills'}/>
                 <div className={s.skills}>
                     {arr.map(t=> <Skill key={t.title} title={t.title} description={t.description}/>)}
                 </div>
